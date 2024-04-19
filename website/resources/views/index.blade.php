f401a6e1b6e1a1b8d59726a102f10aababb3ce47
Nguyen Ngoc Duc <nguyenngocduc.tosa@gmail.com>
2024-04-19 16:26:00 +0700

    update theme

---
+++ website/resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Real estate, Property sale, Property buy">
+    <meta name="description" content="Homy is a beautiful website template designed for Real Estate Agency.">
+    <meta property="og:site_name" content="Homy">
+    <meta property="og:url" content="https://creativegigstf.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Homy-Real Estate HTML5 Template & Dashboard">
+    <meta name='og:image' content='/images/assets/ogg.png'>
+    <!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- For Resposive Device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- For Window Tab Color -->
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#0D1A1C">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#0D1A1C">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#0D1A1C">
+    <title>Homy-Real Estate HTML5 Template & Dashboard</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="56x56" href="/images/fav-icon/icon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" media="all">
+    <!-- Main style sheet -->
+    <link rel="stylesheet" type="text/css" href="/css/style.min.css" media="all">
+    <!-- responsive style sheet -->
+    <link rel="stylesheet" type="text/css" href="/css/responsive.css" media="all">
+
+    <!-- Fix Internet Explorer ______________________________________-->
+    <!--[if lt IE 9]>
+   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+   <script src="vendor/html5shiv.js"></script>
+   <script src="vendor/respond.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+    <div class="main-page-wrapper">
+        @yield('content')
+        <button class="scroll-top">
+            <i class="bi bi-arrow-up-short"></i>
+        </button>
+
+        <!-- Optional JavaScript _____________________________  -->
+
+        <!-- jQuery first, then Bootstrap JS -->
+        <!-- jQuery -->
+        <script src="/vendor/jquery.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- WOW js -->
+        <script src="/vendor/wow/wow.min.js"></script>
+        <!-- Slick Slider -->
+        <script src="/vendor/slick/slick.min.js"></script>
+        <!-- Fancybox -->
+        <script src="/vendor/fancybox/fancybox.umd.js"></script>
+        <!-- Lazy -->
+        <script src="/vendor/jquery.lazy.min.js"></script>
+        <!-- js Counter -->
+        <script src="/vendor/jquery.counterup.min.js"></script>
+        <script src="/vendor/jquery.waypoints.min.js"></script>
+        <!-- Nice Select -->
+        <script src="/vendor/nice-select/jquery.nice-select.min.js"></script>
+        <!-- validator js -->
+        <script src="/vendor/validator.js"></script>
+
+        <!-- Theme js -->
+        <script src="/js/theme.js"></script>
+    </div> <!-- /.main-page-wrapper -->
+</body>
+
+</html>
